2cc1185dfd3079b8dde716423274c10cdaee7174
saintsofearth <apoorvarjaria24@gmail.com>
2024-04-13 11:51:15 +0530

    Stock purchase API endpoints added

--- src/main/java/com/apoorv/transaction/service/controller/transaction.java
+++ src/main/java/com/apoorv/transaction/service/controller/transaction.java
@@ -1,17 +1,35 @@
 package com.apoorv.transaction.service.controller;
 
+import com.apoorv.transaction.service.entity.Purchase;
+import com.apoorv.transaction.service.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("stock")
 @RestController
 public class transaction {
 
+    @Autowired
+    private PurchaseService purchaseService;
+
     @GetMapping("test")
     public ResponseEntity testService() {
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("buy")
+    public Purchase addPurchase(@RequestBody Purchase purchase) {
+        float amountPaid = purchase.getUnits() * purchase.getUnitPrice();
+        purchase.setAmountPaid(amountPaid);
+        return purchaseService.addPurchase(purchase);
+    }
+
+    @GetMapping("user/{userId}/purchase")
+    public List<Purchase> getUserAllPurchase(@PathVariable("userId") String userId) {
+        return purchaseService.getUserAllPurchase(userId);
     }
 }
