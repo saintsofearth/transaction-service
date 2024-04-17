0be1cd38ce580a6528455f58848223a85fe5760b
saintsofearth <apoorvarjaria24@gmail.com>
2024-04-17 20:36:27 +0530

    Config: OpenAPI configuration bean added

---
+++ src/main/java/com/apoorv/transaction/service/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.apoorv.transaction.service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI() {
+        return new OpenAPI().components(new Components())
+                .info(new Info().title("Transaction Service OpenAPI docs")
+                        .version("1.0.0").description("Doc Description"));
+
+    }
+}
